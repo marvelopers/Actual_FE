4219817fd9ded889c328e2416f14176eb9a4159b
marvelopers <marvelopers@gmail.com>
2020-08-07 12:22:57 +0900

    webpack

---
+++ JSX/README.md
@@ -0,0 +1,28 @@
+## webpack부터 setting하는 React
+
+#### webpack 설치
+
+```
+npm i -D webpack webpack-cli wecpack-dev-server
+<!-- 데브서버 실행 : 8080-->
+npx webpack-dev-server
+```
+
+#### ESLint
+
+```
+npm i -D eslint
+npx esLint
+<!-- reset -->
+npx eslint --init
+```
+
+npx webpack-dev-server
+
+## JavaScript Syntax
+
+### Array
+
+- forEach
+- map
+- filter
---
+++ JSX/src/index.js
@@ -0,0 +1,29 @@
+console.log("HELLOW");
+const element = document.getElementById("app");
+
+console.log("element");
+const paragraph = document.createElement("p");
+
+const text = document.createTextNode("LoveU");
+
+paragraph.appendChild(text);
+
+element.appendChild(paragraph);
+
+function createElement(tagName, ...children) {
+  const element = document.createElement(tagName);
+
+  children.forEach((child) => {
+    element.appendChild(child);
+  });
+  element.appendChild(children[0]);
+
+  return element;
+}
+
+// const paragraph = createElement("p", document.createTextNode("hello Wolrd"));
+
+const container = document.getElementById("app");
+const root = document.createElement("div", paragraph);
+
+container.appendChild(paragraph);
