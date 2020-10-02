ac41a962fa514925f232f0e4e9d5c568067156eb
marvelopers <marvelopers@gmail.com>
2020-10-02 23:39:10 +0900

    Learn state&Props

--- HabitList/src/components/Habit.jsx
+++ HabitList/src/components/Habit.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 
 export class Habit extends Component {
-  state = {
-    count: 0,
-  }
-
   handleIncrement = () => {
-    // console.log(event);
-    // 전체적인 state를 변경해줘야 한다.
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   }
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit)
   }
 
   render() {
@@ -28,10 +25,10 @@ export class Habit extends Component {
         <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
-          <i className="fas fa-trash-square"></i>
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
+          <i className="fas fa-trash"></i>
         </button>
-      </li>
+      </li >
     );
   }
 }
--- HabitList/src/components/Habits.jsx
+++ HabitList/src/components/Habits.jsx
@@ -9,10 +9,42 @@ export class Habits extends Component {
       { id: 3, name: 'C', count: 0 },
     ]
   }
+  handleIncrement = (habit) => {
+    // console.log(event);
+    // 전체적인 state를 변경해줘야 한다.
+    // this.setState({ count: this.state.count + 1 });
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  }
+
+  handleDecrement = (habit) => {
+    // const count = this.state.count - 1;
+    // this.setState({ count: count < 0 ? 0 : count });
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  }
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
+    this.setState({ habits });
+  }
+
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (<Habit key={habit.id} habit={habit} />))}
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete} />
+        ))}
       </ul>
     );
   }
