79d5cff8de86003993c2b6a5a0850e7d82606005
marvelopers <marvelopers@gmail.com>
2020-10-03 21:55:52 +0900

    class&function

---
+++ HabitList/src/components/SimpleHabit.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useRef } from "react";
+import { useCallback } from "react";
+
+export const SimpleHabit = (props) => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">Reading</span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+      {/* <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
+        <i className="fas fa-minus-square"></i>
+      </button>
+      <button className="habit-button habit-delete" onClick={this.handleDelete}>
+        <i className="fas fa-trash"></i>
+      </button> */}
+    </li >
+  );
+}
