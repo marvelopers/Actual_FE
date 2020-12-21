7f725cd6ca66171e3567d9f577c43ad099fc4894
marvelopers <marvelopers@gmail.com>
2020-12-21 22:33:53 +0900

    Add middleware 구조 구현

--- ACTUAL_RED_REDUX/index.js
+++ ACTUAL_RED_REDUX/index.js
@@ -5,12 +5,55 @@ console.log(createStore);
 
 const COUNTER = 'count'; // action type
 
+// redux는 동기 처리를 기본으로 한다. 비동기를 처리하기 위해 middleware를 사용한다.
+// function middleware(dispatch, action){
+//   // if(action.type === '비동기 작업'){
+//   //   fetch().then(()=>{
+//   //     dispatch('응답');
+//   //   })
+//   // }
+
+//   return function(action){
+
+//   }
+// } ==>
+
+const middleware = (store)=> (dispatch) => (action) => {
+  if(action.type === fetch) {
+    setTimeout(()=>{
+      dispatch({
+        type: 'fetch-response',
+        payload:[1,2,3]
+      });
+    },2000);
+  }else{
+  dispatch(action)
+  }
+}
+
+const middleware1 = (store)=> (dispatch) => (action) => {
+  dispatch(action)
+}
+
+const middleware2 = (store)=> (dispatch) => (action) => {
+  dispatch(action)
+}
+
+// middleware(dispatch)(action) 요거 커링
+const fn = middleware(dispatch);
+
 function reducer(state, action){
   if(action.type === COUNTER){
     return {...state, counter: action.payload.counter};
   }
+
+  if(action.type === 'fetch-response'){
+    return { ...state, response: action.payload};
+  }
   return state;
 }
+
+fn(action); //미들웨어의 두 번째 함수가 실행되는 시점
 
 function listener(){
   console.log(store.getState());
@@ -23,7 +66,8 @@ function actionCreator(type, payload){
   }
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, [middleware]);
+// const store = createStore(reducer, [middleware1, middleware2]);
 
 store.add(listener);
 
@@ -49,3 +93,5 @@ counter({counter:1});
 
 // dispatch : doUpdate
 // action : data 
+
+store.dispatch(actionCreator('fetch'));
--- ACTUAL_RED_REDUX/redux.js
+++ ACTUAL_RED_REDUX/redux.js
@@ -1,10 +1,11 @@
 
 // 전역 상태를 단방향을 통해 수정하는 것 트리거를 통해 수정 가능
-export function createStore (reducer) {
+export function createStore (reducer, middleware=[]) {
   let state;
   const handler = [];
 
   function dispatch(action){
+    middleware(dispatch, action);
     state = reducer(state, action);
     handler.forEach((listener)=>{
       listener();
@@ -19,10 +20,19 @@ export function createStore (reducer) {
     handler.push(listener)
   }
 
-  return{
-    doUpdate,
+  const store = {
     getState,
     subscribe
+  }
+  middleware = Array.from(middleware).reverse() //순서 역전
+  let lastDispatch = dispatch;
+
+  middleware.forEach(m => {
+    lastDispatch = m(store)(lastDispatch)
+  })
+  return{
+    ...store,
+    dispatch : lastDispatch,
   }
 }
 
